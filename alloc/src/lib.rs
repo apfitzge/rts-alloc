119031a2c180390bc7e0c5dcb506c4d2c89d7d1e
Andrew Fitzgerald <apfitzge@gmail.com>
2025-06-25 19:36:20 +0000

    Header + Visualization

--- alloc/src/lib.rs
+++ alloc/src/lib.rs
@@ -1,1 +1,96 @@
+use std::{os::fd::AsRawFd, path::Path, ptr::NonNull};
 
+pub struct Allocator {
+    header: NonNull<Header>,
+}
+
+impl Allocator {
+    pub fn header(&self) -> &Header {
+        unsafe { self.header.as_ref() }
+    }
+}
+
+pub fn create_allocator(
+    file_path: impl AsRef<Path>,
+    num_workers: u32,
+    slab_size: u32,
+    size: usize,
+) -> Result<Allocator, ()> {
+    let file_path = file_path.as_ref();
+    let file = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create_new(true)
+        .open(file_path)
+        .map_err(|_| ())?;
+    file.set_len(size as u64).map_err(|_| ())?;
+
+    let mmap = unsafe {
+        libc::mmap(
+            core::ptr::null_mut(),
+            size,
+            libc::PROT_READ | libc::PROT_WRITE,
+            libc::MAP_SHARED,
+            file.as_raw_fd(),
+            0,
+        )
+    };
+
+    if mmap == libc::MAP_FAILED {
+        return Err(());
+    }
+
+    let header_ptr = mmap as *mut Header;
+    let mut header = NonNull::new(header_ptr).ok_or(())?;
+
+    unsafe {
+        (*header.as_mut()).magic = MAGIC;
+        (*header.as_mut()).version = VERSION;
+        (*header.as_mut()).num_workers = num_workers;
+        (*header.as_mut()).slab_size = slab_size;
+        (*header.as_mut()).slab_offset = std::mem::size_of::<Header>() as u32;
+    }
+
+    Ok(Allocator { header })
+}
+
+pub fn join_allocator(file_path: impl AsRef<Path>) -> Result<Allocator, ()> {
+    let file_path = file_path.as_ref();
+    let file = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(file_path)
+        .map_err(|_| ())?;
+
+    let size = file.metadata().map_err(|_| ())?.len() as usize;
+
+    let mmap = unsafe {
+        libc::mmap(
+            core::ptr::null_mut(),
+            size,
+            libc::PROT_READ | libc::PROT_WRITE,
+            libc::MAP_SHARED,
+            file.as_raw_fd(),
+            0,
+        )
+    };
+
+    if mmap == libc::MAP_FAILED {
+        return Err(());
+    }
+
+    let header_ptr = mmap as *mut Header;
+    let header = NonNull::new(header_ptr).ok_or(())?;
+    Ok(Allocator { header })
+}
+
+pub const MAGIC: u64 = 0x727473616c6f63; // "rtsaloc"
+pub const VERSION: u32 = 1;
+
+pub struct Header {
+    pub magic: u64,
+    pub version: u32,
+    pub num_workers: u32,
+    pub slab_size: u32,
+    pub slab_offset: u32,
+}
--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,1 +1,152 @@
-fn main() {}
+use std::{io, path::PathBuf};
+
+use clap::{Parser, Subcommand};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::{Constraint, Direction, Layout, Rect},
+    style::Stylize,
+    symbols::border,
+    text::Line,
+    widgets::{Block, Row, Table, Widget},
+    DefaultTerminal, Frame,
+};
+use rts_alloc::Allocator;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Args {
+    /// The path to the allocator file.
+    #[clap(short, long)]
+    path: PathBuf,
+    #[command(subcommand)]
+    subcommand: Option<SubCommand>,
+}
+
+const DEFAULT_SLAB_SIZE: u32 = 2 * 1024 * 1024; // 2 MiB
+
+#[derive(Clone, Debug, Subcommand)]
+enum SubCommand {
+    Create {
+        /// The number of workers.
+        #[clap(long)]
+        num_workers: u32,
+        /// The total size of the allocator.
+        #[clap(long)]
+        file_size: usize,
+        /// The size of each slab.
+        #[clap(long, default_value_t = DEFAULT_SLAB_SIZE)]
+        slab_size: u32,
+        /// Delete existing file if present.
+        #[clap(long, default_value_t = false)]
+        delete_existing: bool,
+    },
+}
+
+fn main() {
+    let Args { path, subcommand } = Args::parse();
+
+    let allocator = match subcommand {
+        Some(SubCommand::Create {
+            num_workers,
+            file_size,
+            slab_size,
+            delete_existing,
+        }) => {
+            if delete_existing && path.exists() {
+                std::fs::remove_file(&path).expect("Failed to delete existing allocator file");
+            }
+            rts_alloc::create_allocator(&path, num_workers, slab_size, file_size)
+                .expect("Failed to create allocator")
+        }
+        None => rts_alloc::join_allocator(&path).expect("Failed to join allocator"),
+    };
+
+    // Setup TUI
+    let mut terminal = ratatui::init();
+    let _ = App {
+        allocator,
+        exit: false,
+    }
+    .run(&mut terminal);
+    ratatui::restore();
+}
+
+pub struct App {
+    allocator: Allocator,
+    exit: bool,
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from("Allocator").bold();
+        let instructions = Line::from(vec!["Press 'q' to quit".into()]);
+        let outer_block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+        let inner = outer_block.inner(area);
+        outer_block.render(area, buf);
+
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints([Constraint::Length(7), Constraint::Min(0)])
+            .split(inner);
+
+        let header = self.allocator.header();
+
+        // -- Header Section
+        let header_rows = vec![
+            Row::new(vec!["magic".into(), format!("0x{:016X}", header.magic)]),
+            Row::new(vec!["version".into(), header.version.to_string()]),
+            Row::new(vec!["num_workers".into(), header.num_workers.to_string()]),
+            Row::new(vec!["slab_size".into(), header.slab_size.to_string()]),
+            Row::new(vec!["slab_offset".into(), header.slab_offset.to_string()]),
+        ];
+        let header_block = Block::bordered().title("Header").border_set(border::PLAIN);
+        let header_table = Table::new(
+            header_rows,
+            &[Constraint::Length(15), Constraint::Length(30)],
+        )
+        .block(header_block)
+        .column_spacing(2);
+        header_table.render(chunks[0], buf);
+    }
+}
