ad2b25f0eee279c58b4a2d08cc5b85d38dc2a31a
Andrew Fitzgerald <apfitzge@gmail.com>
2025-06-27 17:28:34 +0000

    simulate mode

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -18,6 +18,7 @@ struct Args {
 enum SubCommand {
     Allocate { worker_id: u32, size: u32 },
     Free { worker_id: u32, offset: u32 },
+    Simulate { worker_id: u32 },
 }
 
 fn main() {
@@ -68,6 +69,29 @@ fn main() {
                 "Worker {} freed memory at offset {} (0x{:x})",
                 worker_id, offset, offset
             );
+        }
+        SubCommand::Simulate { worker_id } => {
+            let allocator = WorkerAssignedAllocator::new(allocator, worker_id);
+
+            // Choose an arbitrary rotation of sizes to allocate in a loop, then free them.
+            let sizes = [
+                3521, 2186, 171, 2967, 347, 2011, 1552, 3900, 3124, 3857, 1190, 1427, 2856, 2484,
+                2255, 3989, 777, 144,
+            ];
+            let mut allocations = Vec::with_capacity(sizes.len());
+            loop {
+                for size in sizes {
+                    if let Some(ptr) = allocator.allocate(size) {
+                        allocations.push(ptr);
+                    }
+                }
+                // Free all allocations
+                for ptr in allocations.drain(..) {
+                    unsafe {
+                        allocator.free(ptr);
+                    }
+                }
+            }
         }
     }
 }
