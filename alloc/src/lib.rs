9514ffdb206fc2579c021454fb8483fe962a0b07
Andrew Fitzgerald <apfitzge@gmail.com>
2025-07-02 15:07:23 +0000

    remote frees

--- alloc/src/lib.rs
+++ alloc/src/lib.rs
@@ -9,6 +9,7 @@ use crate::{
     align::round_to_next_alignment_of,
     cache_aligned::{CacheAligned, CacheAlignedU32},
     free_stack::FreeStack,
+    remote_free_stack::RemoteFreeStack,
     size_classes::{size_class_index, MAX_SIZE, MIN_SIZE, NUM_SIZE_CLASSES, SIZE_CLASSES},
 };
 
@@ -103,16 +104,60 @@ impl WorkerAssignedAllocator {
 
         // We now know the slab index - we can get the slab metadata.
         let slab_meta = unsafe { self.allocator.slab_meta(slab_index).as_mut() };
+        let size_class_index = slab_meta.size_class_index as usize;
+        let slab_size = self.allocator.header().slab_size;
+        let slab_size_class = SIZE_CLASSES[size_class_index];
+        let allocation_index_in_slab = (offset_from_slab_start / slab_size_class as usize) as u16;
+
         // Check if the slab is assigned to this worker.
         if slab_meta.assigned_worker != self.worker_index as u32 {
-            unimplemented!("remote free not implemented yet");
+            slab_meta.remote_free_stack.push(
+                u32::from(allocation_index_in_slab),
+                slab_size,
+                unsafe { self.allocator.slab(slab_index).as_ptr() },
+            );
+            return;
         }
 
         // Local free - push the item back to the slab's free stack.
+        self.local_free(slab_index, allocation_index_in_slab);
+    }
+
+    /// Drain all remote frees for this worker.
+    pub fn drain_remote_frees(&self) {
+        for size_index in 0..NUM_SIZE_CLASSES {
+            self.drain_remote_frees_for_size_class(size_index as u8);
+        }
+    }
+
+    pub fn drain_remote_frees_for_size_class(&self, size_class_index: u8) {
+        let worker_state = unsafe { self.allocator.worker_state(self.worker_index).as_mut() };
+        let partial_head =
+            worker_state.partial_slabs_heads[usize::from(size_class_index)].load(Ordering::Acquire);
+        for slab_index in unsafe { worker_local_list::iterate(&self.allocator, partial_head) } {
+            unsafe {
+                for index in self.allocator.drain_remote_frees(slab_index) {
+                    self.local_free(slab_index, index as u16);
+                }
+            }
+        }
+
+        let full_head =
+            worker_state.full_slabs_heads[usize::from(size_class_index)].load(Ordering::Acquire);
+        for slab_index in unsafe { worker_local_list::iterate(&self.allocator, full_head) } {
+            unsafe {
+                for index in self.allocator.drain_remote_frees(slab_index) {
+                    self.local_free(slab_index, index as u16);
+                }
+            }
+        }
+    }
+
+    unsafe fn local_free(&self, slab_index: u32, allocation_index_in_slab: u16) {
+        let slab_meta = unsafe { self.allocator.slab_meta(slab_index).as_mut() };
         let size_class_index = slab_meta.size_class_index as usize;
-        let slab_size = self.allocator.header().slab_size as usize;
+        let slab_size = self.allocator.header().slab_size;
         let slab_size_class = SIZE_CLASSES[size_class_index];
-        let allocation_index_in_slab = (offset_from_slab_start / slab_size_class as usize) as u16;
 
         debug_assert!(
             allocation_index_in_slab
@@ -123,11 +168,29 @@ impl WorkerAssignedAllocator {
         let free_stack = &mut slab_meta.free_stack;
         free_stack.push(allocation_index_in_slab);
 
-        let free_stack_capacity = slab_size / slab_size_class as usize;
+        let free_stack_capacity = slab_size as usize / slab_size_class as usize;
+
+        // If the free stack was empty before this push, we must:
+        // 1. Remove the slab from the full list.
+        // 2. Push the slab onto the worker's partial list.
+        let free_stack_len = free_stack.len();
+        let worker_state = unsafe { self.allocator.worker_state(self.worker_index).as_mut() };
+        if free_stack_len == 1 {
+            worker_local_list::remove_slab_from_list(
+                &self.allocator,
+                &worker_state.full_slabs_heads[size_class_index],
+                slab_index,
+            );
+            worker_local_list::push_slab_into_list(
+                &self.allocator,
+                &worker_state.partial_slabs_heads[size_class_index],
+                slab_index,
+            );
+        }
         // If the free stack is now full (i.e. the slab is empty), we must:
         // 1. Remove the slab from the partial list.
         // 2. Push the slab onto the global free stack.
-        if free_stack.len() == free_stack_capacity as u16 {
+        else if free_stack.len() == free_stack_capacity as u16 {
             worker_local_list::remove_slab_from_list(
                 &self.allocator,
                 &self
@@ -196,6 +259,14 @@ impl Allocator {
                 current_head = full_head.load(Ordering::Acquire);
             }
         }
+    }
+
+    /// Should only be called by the worker that owns the slab.
+    pub unsafe fn drain_remote_frees(&self, slab_index: u32) -> impl Iterator<Item = u32> + '_ {
+        let slab_meta = unsafe { self.slab_meta(slab_index).as_mut() };
+        let slab_size = self.header().slab_size;
+        let slab_ptr = unsafe { self.slab(slab_index).as_ptr() };
+        slab_meta.remote_free_stack.drain(slab_size, slab_ptr)
     }
 
     /// Given a slab index, return a pointer to the slab metadata.
@@ -382,7 +453,7 @@ pub struct SlabMeta {
     pub next: u32,            // used for intrusive linked-lists (global_free_stack, worker_local)
     pub assigned_worker: u32, // worker assigned to this slab (if any)
     pub size_class_index: u8, // index into SIZE_CLASSES
-    pub _padding: [u8; 51],   // padding to align to 64 bytes
+    pub remote_free_stack: RemoteFreeStack,
     pub free_stack: FreeStack,
 }
 
@@ -390,6 +461,7 @@ impl SlabMeta {
     pub fn assign(&mut self, slab_size: u32, worker: u32, size_class_index: u8) {
         self.assigned_worker = worker;
         self.size_class_index = size_class_index;
+        self.remote_free_stack.reset();
         unsafe {
             self.free_stack
                 .reset(Self::capacity(slab_size, size_class_index));
@@ -498,6 +570,22 @@ mod worker_local_list {
             head.store(next_slab_index, Ordering::Release);
         }
         unlink_slab_from_list(allocator, slab_index);
+    }
+
+    /// Iterate over the slabs in a worker's list.
+    pub unsafe fn iterate(
+        allocator: &Allocator,
+        mut current_head: u32,
+    ) -> impl Iterator<Item = u32> + '_ {
+        std::iter::from_fn(move || {
+            if current_head == NULL {
+                return None;
+            }
+            let slab_meta = unsafe { allocator.slab_meta(current_head).as_ref() };
+            let slab_index = current_head;
+            current_head = slab_meta.next;
+            Some(slab_index)
+        })
     }
 
     /// Unlink slab from the worker-local free list (double linked list).
